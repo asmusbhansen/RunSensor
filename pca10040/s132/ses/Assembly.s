da9cb709c2097add050ce58b12b8078f7dad9a7b
Filipmathias <filipmathias@live.dk>
2019-04-08 12:09:37 +0200

    Update Assembly.s

--- pca10040/s132/ses/Assembly.s
+++ pca10040/s132/ses/Assembly.s
@@ -14,3 +14,37 @@ compFilt:                     // Function "smuad" entry point.
          ASR r0, #15
          BX       lr           // Return by branching to the address in the link register.
         .fnend
+
+.globl   fATAN2
+.p2align 2
+.type    fATAN2,%function 
+fATAN2:
+        .fnstart
+         //put abs af y i r2
+         cmp     r0, #0
+         rsblt   r2, r0, #0
+         // put abs af x i r3
+         cmp     r1, #0
+         rsblt   r3, r1, #0
+
+         //---------------perform 90 deg rotation 
+         //compare |y|,|x|
+         cmp r2, r3 
+         // r2, r3 is now free
+         //if y greater than x
+         movGT r2,r1 // save x temporarely
+         movGT r1,r0 // save y in x
+         rsbGT r0,r2,#0 // save -x in y
+         
+         //load r2 with add value depending on whether or not a rotation was performed
+         LDRGT r2, #16384
+         LDRLEQ r2, #0
+
+         //divide y/x
+         ASL R0, #16
+         sdiv r0, r0,r1
+ 
+         
+         
+         BX       lr           // Return by branching to the address in the link register.
+ .fnend
